8f4035be34ef271036e74dc26d2648353efaf1d6
NathanRazaf <natrazaf2020@gmail.com>
2024-12-03 13:09:34 -0500

    add cors and host to app

--- server/app.py
+++ server/app.py
@@ -2,8 +2,11 @@ from flask import Flask, request, jsonify
 from data_scraper import get_top_50_country_usernames
 import requests
 import json
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 called_api = "https://calc-osu-plays.onrender.com/fetch/user/scores"
 
@@ -42,4 +45,4 @@ def call_api(username):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
